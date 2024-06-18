bfc6abc91910fcf7fa4eda6c1cd35e51e280271f
jah708 <andrea.16.henao@hotmail.com>
2024-06-18 12:36:47 -0500

    mentoria1 tareas

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Datos Tarea2
+let nombre ="Dilan Rosero"
+let horastrabajadas =8
+let valorporhora =5400
+// let calcular =0
+
+//Procedimiento
+const calcular =(horastrabajadas * valorporhora)/30
+
+// Salida
+console.log('calcular el sueldo del empleado ${horastrabajadas} ${valorporhora} es;' , calcular, "pts")
+
+// Datod tarea3
+let name ="estacionamiento"
+let precioporhora =2000
+let horas =7
+// let cobro=0
+
+// Procedimiento
+const cobro =(precioporhora * horas)
+
+// Salida
+console.log('determinar el precio ${precioporhora} ${horas} es:' , cobro, "pts")
